6f58afb9e94c4c7db16665f07462bcd1208db931
Bruno Smarsaro Bazelato <brunosmarsaro@gmail.com>
2017-11-23 17:36:00 -0200

    area

--- optimizer.py
+++ optimizer.py
@@ -1,4 +1,4 @@
-from math import exp
+from math import exp, sqrt, pi
 from random import randint
 
 class GA:
@@ -12,20 +12,52 @@ class GA:
 		self.altura = param.altura
 
 		self.populacao = self.init_populacao() #mudar para uma esttutura de dados onde tem um evaluation value associado ao individuo
+		print(self.fitness_function(self.populacao[0][0]))
+		
 
 	def run(self):
 		while(True):
 			# Evaluation
 			#for i in range(self.n_geracoes):
-			pass
+			break
 
 	def fitness_function(self, individuo):
-		return ((self.area_coberta(individuo)**2)*(exp(-self.sumpot(individuo))))/self.n_antenas
+		return ((self.area_coberta(individuo)**2)*(self.sumpot(individuo))*(self.med_dist_antenas(individuo)))/(self.n_antenas)
 
 	def area_coberta(self, individuo): #TODO
-		return 1
+		area_total = 0
+		for antena in individuo:
+			r = self.get_raio(antena)
+			area_total += pi*r**2
+		
+		area_total = (area_total/(self.altura*self.largura))*100
+		return area_total
 
-	def sumpot(individuo):
+	def get_raio(self, antena):
+		# CONSTANTES
+		la = 30
+		gr = 20
+		gt = 25
+		pr = 3
+		pt = antena.pot
+
+		r = 1/(((4*pi)/la) * (sqrt(pr/(pt*gr*gt))))
+		return r
+
+	def med_dist_antenas(self, individuo):
+		if self.n_antenas == 1: 
+			return 1
+		dist_total = 0
+		for antena in individuo:
+			for dif_antena in [x for x in individuo if x != antena]:
+				dist_total += sqrt((antena.x - dif_antena.x)**2 + (antena.y - dif_antena.y)**2)
+
+		n_dists = ((self.n_antenas*(self.n_antenas - 1))/2)
+		dist_total = (dist_total/2)/n_dists
+
+		return dist_total
+
+	def sumpot(self, individuo):
 		soma = 0
 		for antena in individuo:
 			soma += antena.pot
@@ -47,6 +79,7 @@ class GA:
 			antena = Antena(x,y,pot)
 			individuo.append(antena)
 
+		individuo = (individuo,0)
 		return individuo
 
 class Antena:
