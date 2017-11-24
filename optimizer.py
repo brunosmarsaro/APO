6db9bc977b193866e460996c1bb03831ba99ff53
Bruno Smarsaro Bazelato <brunosmarsaro@gmail.com>
2017-11-24 17:36:14 -0200

    fiiim

--- optimizer.py
+++ optimizer.py
@@ -12,43 +12,73 @@ class GA:
 		self.altura = param.altura
 
 		self.populacao = self.init_populacao()		
+		#print(self.fitness_function(self.populacao[0][0]))
 
 	def run(self):
 		cont = 0
 		while(True):
-			self.avaliar_populacao()
+			total_fitness = self.avaliar_populacao()
+			self.ordenar_populacao()
 			if cont == self.n_geracoes:
 				break
 			cont += 1
-			self.ordenar_populacao()
-			self.selecionar()
-			# SELECIONAR
-			# APLICAR OPERADORES GENETICOS
-
-	def selecionar(self): #TODO: verificar e terminar
+			self.selecionar(total_fitness)
+			self.mutar()
+			
+	def selecionar(self, total_fitness): #TODO: verificar e terminar
+		norm_fitness = [f[1]/total_fitness for f in self.populacao]
+		probs = [sum(norm_fitness[:i+1]) for i in range(len(norm_fitness))]
+		
 		nova_populacao = []
-		continua_m = int(0.3*self.n_individuos)
-		continua_p = int(0.15*self.n_individuos) + 1
-		for i in range(continua_m):
-			nova_populacao.append(self.populacao[i])
-		for i in range(1, continua_p):
-			nova_populacao.append(self.populacao[self.n_individuos - i])
+		cont = 20
 
+		for i in range(cont):
+			nova_populacao.append(self.populacao[i])
+
+		while(cont < self.n_individuos):
+			r1 = random()
+			novo_i1 = self.gen_individuo()
+			novo_i2 = self.gen_individuo()
+			r2 = random()
+			nf1 = nf2 = False
+			for (i, individuo) in enumerate(self.populacao):
+				if (r1 <= probs[i]  and not nf1):
+					novo_i1 = individuo
+					nf1 = True
+					continue
+				if (r2 <= probs[i] and not nf2):
+					novo_i2 = individuo
+					nf2 = True
+
+				if nf1 and nf2:
+					break
+			novo_i1, novo_i2 = self.crossover(novo_i1[0], novo_i2[0])
+			nova_populacao.append(novo_i1)
+			nova_populacao.append(novo_i2)
+			cont += 2
+
+		self.populacao = nova_populacao
+
+	def mutar(self):
+		for i in range(self.n_individuos):
+			if random() < self.p_mutacao:
+				self.mutacao(self.populacao[i][0])
 
 	def ordenar_populacao(self):
 		self.populacao.sort(key=lambda x: x[1], reverse=True)
 
 	def crossover(self, individuo1, individuo2): #TODO
-		pass
+		i = randint(0, self.n_antenas-1)
+		novo_i1 = individuo1[:i] + individuo2[i:]
+		novo_i2 = individuo2[:i] + individuo1[i:]
+
+		return [novo_i1,0], [novo_i2,0]
 
 	def mutacao(self, individuo): #TODO: Verificar
-		i = randint(0,self.n_antenas-1)
-		if random() < 0.3:
-			individuo[i].x = randint(0,self.largura)
-		if random() < 0.3:
-			individuo[i].y = randint(0,self.altura)
-		if random() < 0.2:
-			individuo[i].pot = randint(1,90)
+		i = randint(0, self.n_antenas-1)
+		individuo[i].x = randint(0,self.largura)
+		individuo[i].y = randint(0,self.altura)
+		individuo[i].pot = randint(1,90)
 
 		return individuo
 			
@@ -58,13 +88,15 @@ class GA:
 			valor_fitness = self.fitness_function(self.populacao[i][0])
 			total_fitness += valor_fitness
 			self.populacao[i][1] = valor_fitness
-
+		'''
 		for i in range (self.n_individuos):
 			valor_fitness = self.populacao[i][1]/total_fitness
 			self.populacao[i][1] = valor_fitness
+		'''
+		return total_fitness
 
 	def fitness_function(self, individuo):
-		return ((self.area_coberta(individuo)**2)*(self.sumpot(individuo))*(self.med_dist_antenas(individuo)))/(self.n_antenas)
+		return ((self.area_coberta(individuo)**3)*(self.sumpot(individuo)))/(self.n_antenas*(self.med_dist_antenas(individuo)))
 
 	def area_coberta(self, individuo): #TODO
 		area_total = 0
@@ -73,17 +105,19 @@ class GA:
 			area_total += pi*r**2
 		
 		area_total = (area_total/(self.altura*self.largura))*100
+
 		return area_total
 
 	def get_raio(self, antena):
 		# CONSTANTES
-		la = 30
-		gr = 20
-		gt = 25
-		pr = 3
+		la = 0.125
+		gr = 63
+		gt = 300
+		pr = 0.018 # 18 mW
 		pt = antena.pot
 
 		r = 1/(((4*pi)/la) * (sqrt(pr/(pt*gr*gt))))
+		#print(r)
 		return r
 
 	def med_dist_antenas(self, individuo):
@@ -115,17 +149,14 @@ class GA:
 	def gen_individuo(self):
 		individuo = []
 		for i in range(self.n_antenas):
-			x = randint(0,self.largura)
-			y = randint(0,self.altura)
-			pot = randint(1,90) #db
-			antena = Antena(x,y,pot)
+			antena = Antena(self.largura, self.altura)
 			individuo.append(antena)
 
 		individuo = [individuo,0]
 		return individuo
 
 class Antena:
-	def __init__(self, x, y, pot):
-		self.x = x
-		self.y = y
-		self.pot = pot
+	def __init__(self, largura, altura):
+		self.x = randint(0,largura)
+		self.y = randint(0,altura)
+		self.pot = randint(1,90) #db
--- run.py
+++ run.py
@@ -1,9 +1,31 @@
+import matplotlib.pyplot as plt
+
 from parserf import Param
 from optimizer import GA
 
 PARAM_FILE = "param.in"
 
+
+def print_area(ga):
+	circulos = []
+	for antena in ga.populacao[0][0]:
+		r = ga.get_raio(antena)
+		print(r)
+		circulo = plt.Circle((antena.x, antena.y), r, edgecolor='black')
+		circulos.append(circulo)
+	fig, ax = plt.subplots()
+	ax.set_xlim((0, ga.largura))
+	ax.set_ylim((0, ga.altura))
+	for circulo in circulos:
+		ax.add_artist(circulo)
+
+	fig.savefig('plot.png')
+
 if __name__ == "__main__":
 	p = Param(PARAM_FILE)
 	ga = GA(p)
 	ga.run()
+	print(ga.populacao[0][1])
+	for antena in ga.populacao[0][0]:
+		print(antena.x, antena.y, antena.pot)
+	print_area(ga)
